1547352e79e7b474d0e00138a6fc6ceeb822cbfc
Ankit Paswan <ankitpaswan192@gmail.com>
2024-02-23 14:59:58 +0530

    js variables

---
+++ javascript/variables.js
@@ -0,0 +1,35 @@
+// 23/02/24
+
+//variable declaration and defination
+
+//var:- it is part of ES5
+var name = 'Ankit';
+
+name = "Paswan";  //re-initilization
+
+console.log(name);
+
+//let variable
+//it is the part of ES6
+
+// let variable = 'let variable';
+let variable = 'let variable'; // the main diff bwt let and var :- we cannot use the same varible name twice
+console.log(variable)
+
+
+//const variable
+
+const PI = 3.14;
+// PI = 3;
+
+console.log(PI);
+
+
+
+//variable naming
+
+let $name = 'ankit'
+let _name = 'ankit'
+let first_name = 'ankit'
+
+console.log(first_name);
