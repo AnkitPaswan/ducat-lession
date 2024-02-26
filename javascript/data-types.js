0813bdb9da541b8f8ea2d666a55d982c8245c89e
Ankit Paswan <ankitpaswan192@gmail.com>
2024-02-26 12:41:47 +0530

    data-types

---
+++ javascript/data-types.js
@@ -0,0 +1,54 @@
+// 26/02/2024
+
+
+//number
+
+let number = 10;
+console.log(typeof number);
+
+console.log(number);
+
+//bigint:- it is introduced in ES-2020
+let bigNumber = BigInt('987654321987654321'); //1st method
+// var bigNumber = 11111111111111111111111111n; //2nd method
+console.log(typeof bigNumber)
+
+console.log(bigNumber);
+
+//boolean
+
+let bool = true;
+
+console.log(typeof bool, bool);
+
+// undefined :- (global execution context)
+let number1 = undefined;
+console.log(typeof number1, number1);
+
+//null
+
+let value = null;
+console.log(typeof value, value);
+
+//array
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(typeof arr, arr);
+
+console.log(arr[2]);
+
+//string
+
+let string = "Hello World";
+// let first_name = "Ankit\A";
+// let lastName = 'Ankit\'A';
+// let first_name = "Ankit\npaswan";
+// let first_name = "Ankit\tpaswan";
+
+console.log(typeof string, string[0]);
+
+
+//mutable
+
+
+//immutable
+
