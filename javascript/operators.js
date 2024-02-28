3006286d4902a4cdcac137e8a2970d1b2d548b3f
Ankit Paswan <ankitpaswan192@gmail.com>
2024-02-28 12:47:51 +0530

    operators

---
+++ javascript/operators.js
@@ -0,0 +1,73 @@
+
+// 28/02/24
+
+//Arithmetic operators
+// let a = 20;
+// let b = 20;
+// console.log(a + b);
+
+//sub operators
+// let a = 20;
+// let b = 20;
+// console.log(a - b);
+
+//mul operators
+// let a = 20;
+// let b = 20;
+// console.log(a * b);
+
+//div operators
+// let a = 20;
+// let b = 20;
+// console.log(a / b);
+
+//exp operators
+let a = 20;
+let b = 20;
+console.log(a ** b);
+
+
+// string concatenation
+
+let fname = "Ankit";
+let lname = "paswan";
+
+let fullname = fname + " " + lname;
+console.log(fullname);
+
+
+//numeric conversion, 
+
+let str = "10";
+console.log(str, typeof str);
+
+let numeric = +str;
+console.log(numeric, typeof numeric);
+
+
+// increment
+let x = 12;
+// x++;
+// x = x + 1;
+//postfix
+// console.log(y++);
+
+// prefix
+// console.log(++x);
+// console.log(a)
+
+//decrement
+
+let y = 10;
+// y = y - 1;
+// y++;
+
+// postfix
+// console.log(x++);
+
+//prefix
+// console.log(--y);
+// console.log(y);
+
+let z = 10;
+console.log(z++ - ++z + --z + z-- + z);
